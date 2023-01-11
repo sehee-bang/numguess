6db5bf4a51c51b78bdc1cc78f0633190a696418f
sehee-bang <i0475833@naver.com>
2023-01-11 10:05:51 +0900

    Modifying Korean to English

--- monty_hall.py
+++ monty_hall.py
@@ -2,8 +2,20 @@
 from random import shuffle, choice
 
 doors=[0,0,1]
-shuffle(doors)
-doors
+result_list=[]
 
-choice(doors)
+for i in range(1,100+1):
+    shuffle(doors)
+    result=choice(doors)
+    result_list.append(result)
 
+sportscar=result_list.count(1)
+sheep=result_list.count(0)
+
+sportscar_aval=sportscar/100
+sheep_aval=sheep/100
+
+print('스포츠를 선택할 확률은 !', end=' ')
+print(sportscar_aval)
+print('양을 선택할 확률은 !', end=' ')
+print(sheep_aval)
