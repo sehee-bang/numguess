c465493bff21dbe25791eb2fb747717312413ff9
sehee-bang <i0475833@naver.com>
2023-01-11 16:15:56 +0900

    Feat:developing numguess games
    function과 recursive를 사용하여 numguess games 도전 기회를 3회로제한하여
    게임을 반복적으로 시행할 수 있도록 한다

--- numguess.py
+++ numguess.py
@@ -1,27 +1,36 @@
 from random import randint
 from time import sleep
 
+#getting random answer
 answer = randint(1,100)
 
+#getting user name
 username = input("Hi, what is your name? >>")
 print("Hi, {}! Please be my guest".format(username))
+print("You can do this game just three times~")
 
-guess = int(input("Just guess the number between 1 and 100 ! >>"))
+#guessing function
+def guessing():
+    guess = int(input("Just guess the number between 1 and 100 ! >>"))
 
-print("So, you picked {}, the answer was {}".format(guess, answer))
+    #Comepare answer with user's guess
+    if guess == answer:
+        print('********************')
+        sleep(1)
+        print('********************')
+        sleep(1)
+        print('********************')
+        sleep(1)
+        print(f'You got it right!! The answer is {answer}!!')
+    elif guess > answer:
+        print(f'Keep going, man~! Too high, {username}..')
+    elif guess < answer:
+        print(f'Keep going, man~! Too Low, {username}..')
 
-#Comepare answer with user's guess
-if guess == answer:
-    print('********************')
-    sleep(1)
-    print('********************')
-    sleep(1)
-    print('********************')
-    sleep(1)
-    print(f'You got it right!! The answer is {answer}!!')
-elif guess > answer:
-    print(f'Keep going, man~! Too high, {username}..')
-elif guess < answer:
-    print(f'Keep going, man~! Too Low, {username}..')
-else:
-    print("Wrong answer!")
+#doing recursive
+i=0
+for i in range(3):
+    guessing()
+    i+=1
+    if i==3:
+        print("The game is over~")
